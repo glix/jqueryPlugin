982b64ece23ebc4b7ee2c50282ffcfe68b3886a3
Danny Glix <danny@dannyglix.com>
2014-04-03 18:29:53 +0530

    Added  Tumblr news ticker

---
+++ Tumblr-News-Ticker/assets/js/script.js
@@ -0,0 +1,11 @@
+$(function() {
+	
+	// Call the plugin
+	
+	$('#main').tumblrNewsTicker({
+		time: 5000,
+		title:  'Amazing jQuery Plugins',
+		blog: 'http://amazing-jquery-plugins.tumblr.com/'
+	});	
+				
+});
